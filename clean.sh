0cd0c5e1b31a90e6f89a9548d6b2190bfbe4b5ae
n9986 <nandeep@miniorb.in>
2012-03-03 20:13:47 +0530

    Lots of changes. CMake files and some scripts.

---
+++ clean.sh
@@ -0,0 +1,12 @@
+rm -f "CMakeCache.txt" 
+rm -f "Makefile" 
+
+rm -f "cmake_install.cmake" 
+rm -f "src/cmake_install.cmake"
+rm -f "test/cmake_install.cmake"
+rm -f "extlibs/tinyxml/cmake_install.cmake"
+
+rm -rf "CMakeFiles"
+rm -rf "src/CMakeFiles"
+rm -rf "test/CMakeFiles"
+rm -rf "extlibs/tinyxml/CMakeFiles"
---
+++ run.sh
@@ -0,0 +1,3 @@
+export DYLD_LIBRARY_PATH="/Users/nandeep/Projects/SFML/lib:/Users/nandeep/Projects/SFML/extlibs/libs-osx/Frameworks/sndfile.framework/Versions/A/"
+
+./test/TrivialTest
