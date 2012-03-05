d95f895dbcf24ce26559b3dade506eade36925a6
n9986 <nandeep@miniorb.in>
2012-03-05 12:32:45 +0530

    Updating the CMakeLists and the run.sh file.

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 export DYLD_LIBRARY_PATH="/Users/nandeep/Projects/SFML/lib:/Users/nandeep/Projects/SFML/extlibs/libs-osx/Frameworks/sndfile.framework/Versions/A/"
 
+ln -s ./test/data data
+
 ./test/TrivialTest
+
+rm -f data
