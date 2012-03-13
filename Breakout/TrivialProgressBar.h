3cf99730ee474c54101f940f11e39d1e73fc6e16
Mayank Saini <mayank.saini@hashstash.in>
2012-03-13 05:32:39 +0530

    Implemented TrivialGUIProgressBar, Ran test case for void ProgressBar::init(bgSprite, repeatSprite, true, true) i.e. Horizontal Progress for positive Direction.

--- Breakout/TrivialProgressBar.h
+++ Breakout/TrivialProgressBar.h
@@ -15,11 +15,13 @@ private:
 	Trivial::Sprite *_repeatSprite;
 	Trivial::Sprite _aRepeatSprite;
 	int _repeatSpritePtr;
+	int _maxSpriteSize;
 	bool _repeatX;
+	bool _direction;
 	float _percentValue;
 public:
 	ProgressBar();
-	void init(const string &bgSprite, const string &repeatSprite, bool repeatX);
+	void init(const string &bgSprite, const string &repeatSprite, bool repeatX, bool direction);
 	~ProgressBar();
 	void setPercentValue(float aValue);
 	virtual void update(){}
@@ -34,7 +36,7 @@ ProgressBar::ProgressBar()
 	_percentValue = 100;
 }
 
-void ProgressBar::init(const string &bgSprite, const string &repeatSprite, bool repeatX = true)
+void ProgressBar::init(const string &bgSprite, const string &repeatSprite, bool repeatX = true, bool positiveDirection = true)
 {
 	_backgroundSprite.image(bgSprite);
 	_aRepeatSprite.image(repeatSprite);
@@ -43,40 +45,77 @@ void ProgressBar::init(const string &bgSprite, const string &repeatSprite, bool 
 	affectChildren();
 	
 	_repeatX = repeatX;
+	_direction = positiveDirection;
 	_repeatSpritePtr = 0;
 	
-	int size = _backgroundSprite.width()/_aRepeatSprite.width();
-	_repeatSprite = new Trivial::Sprite [size];
-				
-	if(_repeatX)
-	{	
-		for(_repeatSpritePtr=0; _repeatSpritePtr < size;  _repeatSpritePtr++)
-		{
-			stringstream append;
-			_repeatSprite[_repeatSpritePtr].image(repeatSprite);
-			append<<"_ProgressBar_repeatSprite"<<_repeatSpritePtr;
-			add(append.str().c_str(), _repeatSprite[_repeatSpritePtr]);
-			_repeatSprite[_repeatSpritePtr].moveTo(X() + _repeatSprite[_repeatSpritePtr].width()*_repeatSpritePtr - _backgroundSprite.width()/2, Y());	
-		}
-		
+	_maxSpriteSize = 0;
+	if(_repeatX){
+		_maxSpriteSize = _backgroundSprite.width()/_aRepeatSprite.width();
 	}
 	else
 	{
-		//_repeatSprite.moveTo(X(), Y() - _backgroundSprite.height()/2);
+		_maxSpriteSize = _backgroundSprite.height()/_aRepeatSprite.height();
 	}
+	_repeatSprite = new Trivial::Sprite [_maxSpriteSize];
+				
+
+	for(_repeatSpritePtr=0; _repeatSpritePtr < _maxSpriteSize;  _repeatSpritePtr++)
+	{
+		stringstream append;
+		_repeatSprite[_repeatSpritePtr].image(repeatSprite);
+		append<<"_ProgressBar_repeatSprite"<<_repeatSpritePtr;
+		add(append.str().c_str(), _repeatSprite[_repeatSpritePtr]);
+		if(_repeatX)
+		{
+			if(_direction)
+			{
+				_repeatSprite[_repeatSpritePtr].moveTo(X() + _repeatSprite[_repeatSpritePtr].width()*_repeatSpritePtr - _backgroundSprite.width()/2, Y());	
+			}
+			else
+			{
+				_repeatSprite[_repeatSpritePtr].moveTo(X() - _repeatSprite[_repeatSpritePtr].width()*_repeatSpritePtr + _backgroundSprite.width()/2, Y());
+			}
+		}
+		else
+		{
+			if(_direction)
+			{
+				_repeatSprite[_repeatSpritePtr].moveTo(X(), Y() + _repeatSprite[_repeatSpritePtr].height()*_repeatSpritePtr - _backgroundSprite.height()/2);
+			}
+			else
+			{
+				_repeatSprite[_repeatSpritePtr].moveTo(X(), Y() - _repeatSprite[_repeatSpritePtr].height()*_repeatSpritePtr + _backgroundSprite.height()/2);
+			}
+		}
+		
+	}
+
 }
 
 void ProgressBar::_update()
 {
+	float floatSize = ((_maxSpriteSize-1) * _percentValue) / 100;
+	int intSize = (int)floatSize + 1;
 	
-	float floatSize = (_repeatSpritePtr * _percentValue) / 100;
-	int intSize = (int)floatSize;
-	
-	for(int i=_repeatSpritePtr; i>0 && i>intSize-1; i--)
+	if(intSize < _repeatSpritePtr-1)
 	{
-		stringstream append;
-		append<<"_ProgressBar_repeatSprite"<<i;
-		remove(append.str().c_str());
+		for(int i=_repeatSpritePtr-1; i>0 && i>intSize; i--)
+		{
+			stringstream append;
+			append<<"_ProgressBar_repeatSprite"<<i;
+			remove(append.str().c_str());
+			_repeatSpritePtr--;
+		}
+	}
+	else
+	{
+		for(int i=_repeatSpritePtr-1; i<intSize && i<_maxSpriteSize; i++)
+		{
+			stringstream append;
+			append<<"_ProgressBar_repeatSprite"<<i;
+			add(append.str().c_str(), _repeatSprite[i]);
+			_repeatSpritePtr++;
+		}
 	}
 	
 	GUIObject::_update();
@@ -85,7 +124,8 @@ void ProgressBar::_update()
 
 void ProgressBar::setPercentValue(float aValue)
 {
-	_percentValue = aValue;
+	if(aValue >= 0 && aValue <= 100)
+		_percentValue = aValue;
 }
 
 ProgressBar::~ProgressBar()
